d25f8930cbdac3d313586aabe0a5422e91490287
Marc Kessler <marc.kesslernyc@gmail.com>
2016-12-29 17:26:26 -0800

    removed unneded iteration in dbl_linked_list

--- src/dbl_linked_list.py
+++ src/dbl_linked_list.py
@@ -60,8 +60,11 @@ class DblLinkedList(object):
         self.tail = None
         self._size = 0
         if maybe_an_iterable:
-            for value in maybe_an_iterable:
-                self.push(value)
+            try:
+                for value in maybe_an_iterable:
+                    self.push(value)
+            except TypeError:
+                self.push(maybe_an_iterable)
 
     def push(self, value):
         """Add a node at the beginning of list and reassign head."""
@@ -76,6 +79,65 @@ class DblLinkedList(object):
             self.tail = self.head
         self._size += 1
 
+    def pop(self):
+        """Remove head node from DLL and return its value."""
+        if self.head is None:
+            raise IndexError("Cannot pop from an empty Double Linked List.")
+        val = self.head.value
+        if self._size == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.nxt
+            self.head.prev = None
+        self._size -= 1
+        return val
+
+    def shift(self):
+        """Remove tail node from DLL and return its value."""
+        if self.tail is None:
+            raise IndexError("Cannot shift from an empty Double Linked List.")
+        val = self.tail.value
+        if self._size == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.nxt = None
+        self._size -= 1
+        return val
+
+    def append(self, value):
+        """Append a new node at the end of list and reassign tail."""
+        new = DblNode(value)
+        if self._size > 0:
+            old = self.tail
+            self.tail = new
+            new.prev = old
+            old.nxt = new
+        else:
+            self.tail = new
+            self.head = self.tail
+        self._size += 1
+
+    def remove(self, value):
+        """Remove the given node from the DLL."""
+        r_node = [node for node in self._iterate_from(self.head) if node.value == value]
+        if not r_node:
+            return ValueError
+        for node in self._iterate_from(self.head):
+            if r_node[0].value == node.value:
+                if self.head.value == r_node[0].value:
+                    self.pop()
+                    return "Succesfully removed Node with value: {0}. New head set to {1}, new size is {2}".format(r_node[0].value, self.head.value, self._size)
+                elif node.nxt is None:
+                    self.shift()
+                    return "Succesfully removed Node with value: {0}. New tail set to {1}, new size is {2}".format(r_node[0].value, self.tail.value, self._size)
+                else:
+                    r_node[0].prev.nxt = r_node[0].nxt
+                    r_node[0].nxt.prev = r_node[0].prev
+                    self._size -= 1
+                    return "Succesfully removed Node with value: {0}. {1} points now to {2} and {3} to {4}, new size is {5}".format(r_node[0].value, r_node[0].prev.value, r_node[0].prev.nxt.value, r_node[0].nxt.value, r_node[0].nxt.prev.value, len(self))
 
     def _iterate_from(self, list_item):
         """Return a generator."""
@@ -83,7 +145,6 @@ class DblLinkedList(object):
             yield list_item
             list_item = list_item.nxt
 
-    def generate_list_of_nodes(self):
-        """Return a list of nodes."""
-        nodes = [node for node in self._iterate_from(self.head)]
-        return nodes
+    def __len__(self):
+        """Return _size of DLL."""
+        return self._size
