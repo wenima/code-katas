4159164c2e18866af8942fcc33155fe65df892b8
Marc Kessler <marc.kesslernyc@gmail.com>
2017-01-08 23:45:14 -0800

    module requires usjon for faster json loads

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='parenthetics.py',
-    description="A module to parse an input string of parenthesis.",
+    name='code-katas',
+    description="A selection of code katas from www.codwars.com.",
     version=0.9,
     author='Marc Kessler-Wenicker',
     author_email='marc.kesslerwenicker@gmail.com',
     license='MIT',
     package_dir={'root': 'src'},
     py_modules=['parenthesis'],
-    install_requires =[],
+    install_requires =['ujson'],
     extras_require={
         'test': ['pytest', 'pytest-cov', 'pytest-watch', 'tox']
     },
